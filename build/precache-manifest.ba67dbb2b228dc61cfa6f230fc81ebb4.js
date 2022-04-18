f5fa6d79b0386809ed57693419373ad0aa92e776
s-sameer <itsnotsameer@gmail.com>
2022-04-18 06:53:22 -0230

    created build

---
+++ build/precache-manifest.ba67dbb2b228dc61cfa6f230fc81ebb4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7737de613b325d800031f2eeb331174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b342d437c2e656857e0e",
+    "url": "/static/js/2.c6509e41.chunk.js"
+  },
+  {
+    "revision": "98a1bcea2545a2ecbf31",
+    "url": "/static/js/main.c443579b.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
